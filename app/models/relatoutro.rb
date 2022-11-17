51ca939efbd3e610d9fc53c4e471e404d9a40c9a
EstherSantAnaGomes <esthersant.edu@gmail.com>
2022-11-17 00:33:00 -0300

    deu erro cm validações do relat/refiz devise views

--- app/models/relatoutro.rb
+++ app/models/relatoutro.rb
@@ -1,7 +1,2 @@
 class Relatoutro < ApplicationRecord
-    validates :parceria_firmada_com, :denominada_estagio, :CNPJ_estagio, :rua_estagio, :numero_estagio, 
-    :periodo_letivo, :bairro_estagio, :municipio_estagio, :telefone_estagio, :representado_por, :ano, 
-    :endereco, :bairro, :municipio, :estado, :CEP, presence: true
-
-    validates :periodo_de, :periodo_a, presence: false
 end
--- app/models/relatparticular.rb
+++ app/models/relatparticular.rb
@@ -1,7 +1,2 @@
 class Relatparticular < ApplicationRecord
-    validates :representado_por, :periodo, :semestre, :ano, :endereco, :bairro, :municipio, 
-    :estado, :cep, :cnpj, :instituicao_apresentacao, :aluno_apresentacao, 
-    :aluno_apresentacao_dois, :semestre_apresentacao, :ano_apresentacao, :aluno_semestre, :estado_da_instituicao, presence: true
-
-    validates :periodo_de, :periodo_a, presence: false
 end
