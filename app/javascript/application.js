925219012b7b190ce3e98cb195d9c80f5e4e7820
EstherSantAnaGomes <esthersant.edu@gmail.com>
2022-11-18 15:52:48 -0300

    mensagens de erro aparecendo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-import "@hotwired/turbo-rails"
+
 import "controllers"
 
 // This is a manifest file that'll be compiled into application.js, which will include all the files
