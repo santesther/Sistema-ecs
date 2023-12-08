7521fdc4f7b3a0b135d43a0c913e42bc930179b8
EstherSantAnaGomes <esthersant.edu@gmail.com>
2023-12-08 18:08:15 -0300

    correção pdf_centro

--- app/pdfs/centro_pdf.rb
+++ app/pdfs/centro_pdf.rb
@@ -8,7 +8,7 @@ class CentroPdf < Prawn::Document
 
         image "#{Rails.root}/app/assets/images/cabecalho.png", at: [-50, 680], :width => 550
         move_down 60
-        move_down 15
+        move_down 5
         text "<b>CARTA DE APRESENTAÇÃO</b>", align: :center, size: 15, :inline_format => true, :leading => 10
         
         repeat(2..7, dynamic: true) do
@@ -18,15 +18,17 @@ class CentroPdf < Prawn::Document
         @relatorio.each do |relatorio|
           if(relatorio.matricula_aluno == @usuario_matricula)
             draw_text "Campos dos Goytacazes, RJ, #{relatorio.data}", at: [200, 600], :inline_format => true
-            move_down 20
-            text "<b>À:</b><b>Direção da Instituição de Ensino:</b> <u>#{relatorio.instituicao_apresentacao}</u>", align: :left, size: 12, :inline_format => true
-            move_down 20
-            text "<b>Assunto: Apresentação do Discente-Estagiário:</b> <u>#{relatorio.aluno_apresentacao}</u>", align: :left, size: 10, :inline_format => true, :leading => 10
             move_down 10
+            text "<b>À:</b><b>Direção da Instituição de Ensino:</b> <u>IFFluminense campus Campos Centro/DAEBPCC</u>", align: :left, size: 12, :inline_format => true
+            move_down 10
+            text "<b>DA:</b><u> Diretoria de Ensino Superior dos Cursos de Licenciatura/Núcleo de Apoio à Prática Profissional (NAPP) do IFFluminense campus Campos Centro</u>", align: :left, size: 12, :inline_format => true
+            move_down 10
+            text "<b>Assunto: Apresentação do Discente-Estagiário:</b> <u>#{relatorio.aluno_apresentacao}</u>", align: :left, size: 10, :inline_format => true, :leading => 10
+            move_down 5
             text "Prezado Gestor.", align: :justify, :inline_format => true, :leading => 10
             move_down 10
             text "O IFFluminense <i>campus</i> Campos Centro apresenta o(a) licenciando(a)-estagiário(a): <u>#{relatorio.aluno_apresentacao_dois}</u>, matriculado(a), neste instituto, no <u>#{relatorio.periodo}</u> período do Curso Superior de Licenciatura em <u>#{relatorio.licenciatura}</u>, no <u>#{relatorio.semestre_apresentacao}</u> semestre letivo de <u>#{relatorio.ano_apresentacao}</u>, para fins de cumprimento das atividades do Estágio Curricular Supervisionado no campo de estágio dessa Instituição Concedente, prescristas no Plano de Atividades de Estágio(PAE), conforme Calendário Acadêmico deste <i>Campus</i>, em atendimento a carga horária total destinada ao período em curso.", align: :left, :inline_format => true, :leading => 10
-            move_down 20
+            move_down 5
             text "Ressaltamos que o Estágio Curricular Supervisionado é componente obrigatório e de importância significativa para a formação dos futuros formadores. Agradecemos a atenção sempre nos concedida e a seriedade com que os profissionais da educação têm acolhido e acompanhado nossos(as) licenciados(as). Sem mais, colocamo-nos à disposição para quaisquer esclarecimentos e registramos, nesta, os contatos com DIRLIC/NAPP (22) 2726-2897 // dirlicenciatura.camposcentro@iff.edu.br, bem como o site licenciaturas.centro.iff.edu", align: :left, :inline_format => true, :leading => 10
 
            
