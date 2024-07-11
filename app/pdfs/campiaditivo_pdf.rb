d6193e571ecd6f7f9d4eab530a5ef003ea4b15c8
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-11 19:49:07 -0300

    mudanças relatorio campi_aditivo

--- app/pdfs/campiaditivo_pdf.rb
+++ app/pdfs/campiaditivo_pdf.rb
@@ -15,88 +15,22 @@ class CampiaditivoPdf< Prawn::Document
 
         @relatorio.each do |relatorio|
           if(relatorio.matricula_aluno == @usuario_matricula)
-            draw_text "Campos dos Goytacazes, RJ, #{relatorio.data}", at: [240, 600], :inline_format => true
-            text "<b>TERMO ADITIVO DE ESTÁGIO</b> (Período letivo: <b>#{relatorio.periodo_letivo})</b>", align: :center, size: 15, :inline_format => true, :leading => 10
+            text "<b>TERMO ADITIVO DE ESTÁGIO</b>", align: :center, size: 15, :inline_format => true, :leading => 10
             text "<b>Seguradora: </b>#{relatorio.seguradora}",align: :center, size: 12, :inline_format => true, :leading => 10
             text "<b>Apólice de seguro nº: </b>#{current_user.apolice} ", align: :center, size: 12, :inline_format => true, :leading => 10
-            move_down 5
-            text "O <b>INSTITUTO FEDERAL FLUMINENSE</b>, CNPJ/MF nº 10.779.511/0001-07, situado à Rua Coronel Walter Kramer, nº 357, bairro: Parque Santo Antônio, município: Campos dos Goytacazes/RJ, CEP: 28080-565, neste Ato representado pela <b>Diretoria de Ensino dos Cursos Superiores de Licenciatura do CAMPUS CAMPOS CENTRO - INSTITUIÇÃO PROMOTORA -</b>, inscrito no CNPJ/MF sob o nº 10.779.511/0002-98, situada na Rua Doutor Siqueira, nº 273, Bairro: Parque Dom Bosco, município: Campos dos Goytacazes/RJ, CEP: 28.030-130, telefone (22) 27262897, <b>firma parceria</b>, em atendimento à Lei Nº 11.788, de 25/09/2008, com o <b>Instituto Federal Fluminense <i>campus<i> #{relatorio.instituicao_apresentacao} - Instituição de Ensino CONCEDENTE -</b> inscrito no CNPJ/MF sob o nº #{relatorio.cnpj}, situado no(a) #{relatorio.endereco_da_instituicao}, Bairro: #{relatorio.bairro_da_instituicao}, no município de #{relatorio.municipio_da_instituicao}, Estado do Rio de Janeiro, CEP: #{relatorio.cep_da_instituicao}, telefone #{relatorio.telefone_da_instituicao}, representado por <b>#{relatorio.representante_da_instituicao}, para fins de continuidade do Estágio Curricular Supervisionado dos Cursos de Licenciatura</b>, ao(à) <b>LICENCIANDO(A)</b>", align: :justify, :inline_format => true, :leading => 10
+            move_down 10
+            text "O <b>INSTITUTO FEDERAL FLUMINENSE</b>, CNPJ/MF nº 10.779.511/0001-07 neste Ato representado por Marlúcia Cereja de Alencar <b>Diretora de Ensino Superior das Licenciaturas - INSTITUIÇÃO PROMOTORA -</b>, inscrita no CNPJ/MF sob o nº 10.779.511/0002-98, situada na Rua Doutor Siqueira, nº 273, Parque Dom Bosco, Campos dos Goytacazes/RJ, CEP: 28.030-130, telefone (22) 27262897, <b>firma parceria</b>, em atendimento à Lei Nº 11.788, de 25/09/2008, com o <b>Instituto Federal Fluminense <i>Campus<i> #{relatorio.instituicao_apresentacao} - Instituição de Ensino CONCEDENTE -</b> inscrito no CNPJ/MF sob o nº #{relatorio.cnpj}, situado no(a) #{relatorio.endereco_da_instituicao}, Bairro: #{relatorio.bairro_da_instituicao}, no município de #{relatorio.municipio_da_instituicao}, Estado do Rio de Janeiro, CEP: #{relatorio.cep_da_instituicao}, telefone #{relatorio.telefone_da_instituicao}, representado por <b>#{relatorio.representante_da_instituicao}, para fins de continuidade do Estágio Curricular Supervisionado dos Cursos de Licenciatura</b>, ao(à) <b>LICENCIANDO(A)</b>", align: :justify, :inline_format => true, :leading => 10
             move_down 10
             text "#{relatorio.aluno_apresentacao}", align: :center, size: 16, :inline_format => true, :leading => 10 
             move_down 10
-            text "matrícula nº #{relatorio.matricula_aluno} do #{relatorio.periodo} do Curso Superior de Licenciatura em #{relatorio.licenciatura}.", align: :justify, :inline_format => true, :leading => 10
+            text "matrícula nº #{relatorio.matricula_aluno} do #{relatorio.semestre_apresentacao } semestre do ano letivo #{relatorio.ano} do Curso Superior de Licenciatura em #{relatorio.licenciatura}.", align: :justify, :inline_format => true, :leading => 10
             
             move_down 30
             text "<b>Cláusula Primeira:</b>", align: :left, :inline_format => true, :leading => 5
-            text "O PRESENTE TERMO ADITIVO ALTERA:", align: :left, :inline_format => true, :leading => 5 
-            text "(<b>X</b>) O período do término do estágio para: <b>#{relatorio.periodo_a}</b>", align: :justify, :inline_format => true, :leading => 5
-            text "<b>Observação:</b> Termo Aditivo => justifica-se pelo(a) discente ter desenvolvido as atividades do Estágio Curricular Supervisionado #{relatorio.periodo_dirlic} período no Campus Campos Centro do IFFluminense, no #{relatorio.semestre_dirlic} semestre letivo de #{relatorio.ano}.", align: :justify, :inline_format => true, :leading => 5
-            
-            if relatorio.avaliador == "Edina Lacerda"
-              bounding_box([-10, cursor], width: 150, height: 100) do
-                transparent(0) { stroke_bounds }
-                %i[left].each do |vposition|
-                image "#{Rails.root}/app/assets/images/carimbo_edina.png", :width => 300,
-                position: :left, vposition: 40
-                end
-                bounding_box([10, cursor], width: 400, height: 450) do
-                  transparent(0) { stroke_bounds }
-                  %i[center].each do |vposition|
-                  image "#{Rails.root}/app/assets/images/assinatura_edina.png", :width => 150,
-                  position: :left, vposition: -50
-              end
-            end
-          end
-            elsif relatorio.avaliador == "Elizabeth Freitas"
-              bounding_box([-10, cursor], width: 150, height: 100) do
-                transparent(0) { stroke_bounds }
-                %i[left].each do |vposition|
-                image "#{Rails.root}/app/assets/images/carimbo_Elizabeth.png", :width => 250,
-                position: :left, vposition: 40
-                end
-                bounding_box([55, cursor], width: 400, height: 450) do
-                  transparent(0) { stroke_bounds }
-                  %i[center].each do |vposition|
-                  image "#{Rails.root}/app/assets/images/assinatura_elizabeth.png", :width => 140,
-                  position: :left, vposition: -90
-              end
-            end
-          end
-          elsif relatorio.avaliador == "Virginia Ribeiro"
-              bounding_box([-10, cursor], width: 150, height: 100) do
-                transparent(0) { stroke_bounds }
-                %i[left].each do |vposition|
-                image "#{Rails.root}/app/assets/images/carimbo_virginia_certo.png", :width => 200,
-                position: :left, vposition: 40
-                end
-                bounding_box([0, cursor], width: 400, height: 450) do
-                  transparent(0) { stroke_bounds }
-                  %i[center].each do |vposition|
-                  image "#{Rails.root}/app/assets/images/assinatura_virginia_ok.png", :width => 220,
-                  position: :left, vposition: -90
-              end
-            end
-          end
-          elsif relatorio.avaliador == "Marlúcia Cereja"
-              bounding_box([-10, cursor], width: 150, height: 100) do
-                transparent(0) { stroke_bounds }
-                %i[left].each do |vposition|
-                image "#{Rails.root}/app/assets/images/carimbo_marlucia_ok.png", :width => 185,
-                position: :left, vposition: 40
-                end
-                bounding_box([55, cursor], width: 400, height: 450) do
-                  transparent(0) { stroke_bounds }
-                  %i[center].each do |vposition|
-                  image "#{Rails.root}/app/assets/images/assinatura_marlucia.png", :width => 140,
-                  position: :left, vposition: -80
-              end
-            end
-          end
-          else 
-            text "Usuário não cadastrado"
-          end
+            text "O PRESENTE TERMO ADITIVO INCLUI:", align: :left, :inline_format => true, :leading => 5 
+            text "<b>(X)</b> O componente curricular #{relatorio.estagio} no semestre do ano letivo #{relatorio.ano}.", align: :justify, :inline_format => true, :leading => 10
 
-            move_down 110
+            move_down 150
             text "CLÁUSULA SEGUNDA", align: :center, size: 16
             move_down 20
             text "Permanecem válidas todas as demais disposições do Termo de Compromisso celebrado, do qual passa a ser integrante este Termo Aditivo.
@@ -113,69 +47,6 @@ class CampiaditivoPdf< Prawn::Document
             text "IFFluminense <i>campus</i> Campos Centro: "
             stroke_horizontal_rule
             pad_top(20) { }
-              if relatorio.avaliador == "Edina Lacerda"
-                bounding_box([65, cursor], width: 150, height: 100) do
-                  transparent(0) { stroke_bounds }
-                  %i[center].each do |vposition|
-                  image "#{Rails.root}/app/assets/images/carimbo_edina.png", :width => 300,
-                  position: :center, vposition: -10
-                  end
-                  bounding_box([55, cursor], width: 400, height: 450) do
-                    transparent(0) { stroke_bounds }
-                    %i[center].each do |vposition|
-                    image "#{Rails.root}/app/assets/images/assinatura_edina.png", :width => 150,
-                    position: :center, vposition: -120
-                end
-              end
-            end
-              elsif relatorio.avaliador == "Elizabeth Freitas"
-                bounding_box([50, cursor], width: 150, height: 100) do
-                  transparent(0) { stroke_bounds }
-                  %i[center].each do |vposition|
-                  image "#{Rails.root}/app/assets/images/carimbo_Elizabeth.png", :width => 250,
-                  position: :center, vposition: 5
-                  end
-                  bounding_box([55, cursor], width: 400, height: 450) do
-                    transparent(0) { stroke_bounds }
-                    %i[center].each do |vposition|
-                    image "#{Rails.root}/app/assets/images/assinatura_elizabeth.png", :width => 140,
-                    position: :center, vposition: -150
-                end
-              end
-            end
-            elsif relatorio.avaliador == "Virginia Ribeiro"
-                bounding_box([-10, cursor], width: 150, height: 100) do
-                  transparent(0) { stroke_bounds }
-                  %i[left].each do |vposition|
-                  image "#{Rails.root}/app/assets/images/carimbo_virginia_certo.png", :width => 200,
-                  position: :left, vposition: -5
-                  end
-                  bounding_box([280, cursor], width: 400, height: 450) do
-                    transparent(0) { stroke_bounds }
-                    %i[center].each do |vposition|
-                    image "#{Rails.root}/app/assets/images/assinatura_virginia_ok.png", :width => 220,
-                    position: :left, vposition: -170
-                end
-              end
-            end
-            elsif relatorio.avaliador == "Marlúcia Cereja"
-                bounding_box([-10, cursor], width: 150, height: 100) do
-                  transparent(0) { stroke_bounds }
-                  %i[left].each do |vposition|
-                  image "#{Rails.root}/app/assets/images/carimbo_marlucia_ok.png", :width => 185,
-                  position: :left, vposition: -5
-                  end
-                  bounding_box([280, cursor], width: 400, height: 450) do
-                    transparent(0) { stroke_bounds }
-                    %i[center].each do |vposition|
-                    image "#{Rails.root}/app/assets/images/assinatura_marlucia.png", :width => 140,
-                    position: :left, vposition: -150
-                end
-              end
-            end
-            else 
-              text "Usuário não cadastrado"
-            end
 
             move_down 30
             text "Licenciando(a) Estagiário(a): ______________________________________________________________", :inline_format => true
