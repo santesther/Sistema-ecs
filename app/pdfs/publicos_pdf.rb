25f217920c3e9e772cda86b26e6d7693f0d2e06a
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-11 18:30:58 -0300

    mudanças relatorio publico

--- app/pdfs/publicos_pdf.rb
+++ app/pdfs/publicos_pdf.rb
@@ -29,9 +29,9 @@ class PublicosPdf < Prawn::Document
             move_down 10
             text "<b>#{relatorio.aluno_apresentacao}</b>", align: :center, size: 16, :inline_format => true, :leading => 10
             move_down 10 
-            text "matriculado(a) no <i>Campus</i> Campos Centro do IFFluminense, sob o nº #{relatorio.matricula_aluno}, no #{relatorio.aluno_semestre} semestre do ano letivo de #{relatorio.ano}, no Curso Superior de Licenciatura em #{relatorio.licenciatura}, no componente curricular #{relatorio.estagio}. Residente no(a) #{relatorio.endereco}, nº #{relatorio.numero}, Complemento: #{relatorio.complemento}, Bairro: #{relatorio.bairro}, município: #{relatorio.municipio}, UF: #{relatorio.estado}, CEP: #{relatorio.cep}, telefone/contato: nº #{relatorio.telefone}", align: :justify, :inline_format => true, :leading => 10
+            text "matriculado(a) no <i>Campus</i> Campos Centro do IFFluminense, sob o nº #{relatorio.matricula_aluno}, no #{relatorio.aluno_semestre} semestre do ano letivo de #{relatorio.ano}, no Curso Superior de Licenciatura em #{relatorio.licenciatura}, no componente curricular #{relatorio.estagio}. Residente no(a) #{relatorio.endereco}, nº #{relatorio.numero}, Complemento: #{relatorio.complemento}, Bairro: #{relatorio.bairro}, município: #{relatorio.municipio}, UF: #{current_user.UF}, CEP: #{relatorio.cep}, telefone/contato: nº #{relatorio.telefone}", align: :justify, :inline_format => true, :leading => 10
             move_down 40
-          
+            text "<b>Período de Vigência: #{relatorio.periodo_de}", align: :justify, :inline_format => true
 
             move_down 130
             text "CLÁUSULA PRIMEIRA - DA FINALIDADE", align: :center, size: 16
