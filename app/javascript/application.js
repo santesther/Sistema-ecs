8900265b206bd124cef3227020a88ecddddb6245
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-12 19:57:18 -0300

    tentativa

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,12 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import Swal from 'sweetalert2';
 window.Swal = Swal;
+
+import { application } from "./application"
+
+import AlertController from "./alert_controller"
+application.register("alert", AlertController)
+
+import HelloController from "./hello_controller"
+application.register("hello", HelloController)
+
