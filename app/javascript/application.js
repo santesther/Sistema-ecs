5694975fea61032b8d6a853070d53eeb4d431c6d
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-12 20:05:51 -0300

    .

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,11 +4,3 @@ import "controllers"
 import Swal from 'sweetalert2';
 window.Swal = Swal;
 
-import { application } from "./application"
-
-import AlertController from "./alert_controller"
-application.register("alert", AlertController)
-
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
