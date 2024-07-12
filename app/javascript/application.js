a5123fbc96aac477ac89da24a63beb444cf9257a
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-12 19:44:15 -0300

    application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
-import "@hotwired/stimulus"
+// Import Turbo and Hotwired controllers
 import "@hotwired/turbo-rails"
 import "controllers"
 import Swal from 'sweetalert2';
 window.Swal = Swal;
-//import { Turbo } from "@hotwired/turbo-rails"
