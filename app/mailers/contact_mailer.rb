a2f79b5e1095215b86cbaf472ddb82177ad0122a
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-12 15:51:02 -0300

    sender correto

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,22 +1,22 @@
 class ContactMailer < ActionMailer::Base
-  default :from => 'ecs-dirlic.centro@mail.iff.edu.br'
+  default :from => 'ecs-dirlic.centro@iff.edu.br'
 
   def contact_message(current_user)
     @current_user = current_user
     # mail(:to => current_user.email, :subject => 'Mensagem de Contato')
-    mail(:to => 'ecs-dirlic.centro@mail.iff.edu.br', :subject => 'Formulário de Estágio')
+    mail(:to => 'ecs-dirlic.centro@iff.edu.br', :subject => 'Formulário de Estágio')
   end
 
   def confirmacao_impressao(current_user)
     @current_user = current_user
     # mail(:to => current_user.email, :subject => 'Mensagem de Contato')
-    mail(:to => 'ecs-dirlic.centro@mail.iff.edu.br', :subject => 'Formulário de Estágio')
+    mail(:to => 'ecs-dirlic.centro@iff.edu.br', :subject => 'Formulário de Estágio')
   end
 
   def ingresso_estagio(current_user)
     @current_user = current_user
     # mail(:to => current_user.email, :subject => 'Mensagem de Contato')
-    mail(:to => 'ecs-dirlic.centro@mail.iff.edu.br', :subject => 'Formulário de Estágio')
+    mail(:to => 'ecs-dirlic.centro@iff.edu.br', :subject => 'Formulário de Estágio')
   end
 
   def mudanca_status(user)
