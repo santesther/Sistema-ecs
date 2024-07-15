879c98e9be3cd1a93c90b0e94501e5355df467bd
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-15 20:32:44 -0300

    tentativa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
