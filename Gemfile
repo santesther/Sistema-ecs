0b3f8619f59297f6ae3e512605fb36ea223020e3
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-15 19:20:34 -0300

    remove webpacker

--- Gemfile
+++ Gemfile
@@ -121,5 +121,3 @@ end
 gem 'pg', '~> 1.4', '>= 1.4.6', group: :production
 
 gem "cssbundling-rails"
-
-gem 'webpacker'
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,8 @@
 const { environment } = require('@rails/webpacker')
+const { resolve } = require('path')
+
+environment.config.set('entry', {
+  application: resolve(__dirname, '../../app/javascript/controllers/application.js')
+})
 
 module.exports = environment
