18b5c04cff65dad3d78e1241e861b7a454412a97
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-15 17:24:04 -0300

    js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -8,5 +8,49 @@ window.Stimulus   = application
 
 export { application }
 
+jQuery(function() { 
+    $('.clausulas').hide();
+    $('.conteudo').show();
+  
+    $('.confirmacao_preenchimento').slideDown(300);
+    $('.clausulas_conteudo').slideDown(300);
+    $('.clausulas_conteudo').css("background-color", "rgba(10,10,10,0.6)");
+  
+    $(document).on('click', '.confirma_preenchimento', function() {
+        $('.confirmacao_preenchimento').slideUp(300);
+        $('.clausulas_conteudo').slideUp(300);
+    });
+    
+    $(document).on('click', '.avancar', function() {
+        $('.clausulas').show(800);
+        $('.clausulas_conteudo').show(800);
+        $('.clausulas_conteudo').css("background-color", "rgba(10,10,10,0.6)");
+    });
+  
+    $(document).on('click', '.cancelar', function() {
+        $('.clausulas').hide(800);
+        $('.clausulas_conteudo').hide(800);
+    });
+  
+    $(document).on('mouseover', '.btn1', function() {
+        $('.div_federal').slideDown();
+    });
+  
+    $(document).on('mouseleave', '.div_federal', function() {
+        $('.div_federal').slideUp();
+    });
+  
+    $(document).on('mouseover', '.btn2', function() {
+        $('.div_federal2').slideDown();
+    });
+  
+    $(document).on('mouseleave', '.div_federal2', function() {
+        $('.div_federal2').slideUp();
+    });
+  
+    $(document).on('click', '.desaparece_botao', function() {
+        $('.desaparece_botao').hide(700);
+    });
+  });
 
  
