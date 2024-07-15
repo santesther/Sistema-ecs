8b00a6d35a9fa9075cc181a05970e3ad3d9b7786
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-15 19:23:39 -0300

    webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const { resolve } = require('path')
-
-environment.config.set('entry', {
-  application: resolve(__dirname, '../../app/javascript/controllers/application.js')
-})
 
 module.exports = environment
