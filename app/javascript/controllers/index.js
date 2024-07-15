d72ebb69cbde6cb51b363ad0e4714c53e3f02a33
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-15 18:40:16 -0300

    ,..

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,10 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "../controllers"
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+eagerLoadControllersFrom("controllers", application)
+
+import { application } from "./application"
 
 import AlertController from "./alert_controller"
 application.register("alert", AlertController)
