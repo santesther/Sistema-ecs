fb458042dadfbf6a6b9b5f6d75f40c7a97400cda
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-15 20:25:04 -0300

    tentativa import

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,9 @@
-import { application } from "controllers/application"
+import { application } from "../controllers/application"
+
+import { application } from "./application"
+
+import AlertController from "./alert_controller"
+application.register("alert", AlertController)
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
