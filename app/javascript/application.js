a2ae5dfc63c152d11baaa672bfdda9f83baf8146
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-16 15:29:25 -0300

    .

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,7 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/stimulus"
+import "@hotwired/turbo-rails"
+import "controllers"
+import Swal from 'sweetalert2';
+window.Swal = Swal;
+
+import { Turbo } from "@hotwired/turbo-rails"
