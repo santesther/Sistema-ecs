14338248c674d70b7d55c621bd8baf9a03255b85
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-16 09:26:47 -0300

    tentativa production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,8 +33,9 @@ Rails.application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
+  config.assets.precompile += [/.*\.png/,/.*\.ico/,/.*\.jpg/,/.*\.js/,/.*\.scss/]
   # Generate digests for assets URLs
   config.assets.digest = true
 
