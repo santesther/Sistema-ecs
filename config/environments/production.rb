bbf50e1a53a30408df1a1c9d1f2dc88dd4bdc67f
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-16 11:35:01 -0300

    resolução conflitos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,13 +33,13 @@ Rails.application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
