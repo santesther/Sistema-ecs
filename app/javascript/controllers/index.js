048971c8eccec7d68562d50a440f85cd83e9aa6c
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-16 10:27:28 -0300

    mudanças

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,6 +6,3 @@ import { application } from "./application"
 
 import AlertController from "./alert_controller"
 application.register("alert", AlertController)
-
-//import HelloController from "./hello_controller"
-//application.register("hello", HelloController)
