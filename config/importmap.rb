b3e6a900929eaf1e274eff30edcb43e248f6ba5e
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-16 15:05:30 -0300

    importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,12 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 
 pin "sweetalert2", to: "https://ga.jspm.io/npm:sweetalert2@11.10.4/dist/sweetalert2.all.js"
-pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.js"
-
