3e611967eef310b217ee52ddfcbcea386ba84f11
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-16 09:27:35 -0300

    revertendo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,9 +33,8 @@ Rails.application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
-  config.assets.precompile += [/.*\.png/,/.*\.ico/,/.*\.jpg/,/.*\.js/,/.*\.scss/]
   # Generate digests for assets URLs
   config.assets.digest = true
 
