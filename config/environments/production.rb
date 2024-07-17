3e8707d613d149d2807127e06a0f9f6d6a83219c
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-17 17:37:01 -0300

    mailer production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { :host => ENV['ecs-dirlic.centro@iff.edu.br'], 
+    only_path: false }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
