3ebeac21a44c88931cd5ad9cc0709d77f48c397a
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-17 18:52:35 -0300

    envio mensagem p aluno

--- app/models/mensagem.rb
+++ app/models/mensagem.rb
@@ -1,2 +1,8 @@
 class Mensagem < ApplicationRecord
+    belongs_to :remetente, class_name: "User"
+    belongs_to :destinatario, class_name: "User"
+  
+    validates_presence_of :texto
+    
+    self.per_page = 10
 end
