b406df44753fdb27646fd014dac50442243efb84
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-18 18:41:14 -0300

    after signout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < ActionController::Base
   
     protected
 
+    def after_sign_out_path_for scope
+      root_path
+    end
    
 	def configure_permitted_parameters     
     devise_parameter_sanitizer.permit(:sign_up) do |users_params|        
