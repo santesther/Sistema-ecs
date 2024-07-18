7bcfc25f6934b521125d19ee22d3d327f1350aec
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-18 18:05:58 -0300

    authentication login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
     :address => "mail.iff.edu.br",
     :port => 587,
     :domain => "iff.edu.br",
-    :authentication => "plain",
+    :authentication => :login,
     :user_name => "ecs-dirlic.centro@iff.edu.br",
     :password => "licenciatura2024#",
     :enable_starttls_auto => true,
