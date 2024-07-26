d91960b8df411980497e79b0d141f310c6643bab
EstherSantAnaGomes <esthersant.edu@gmail.com>
2024-07-26 15:28:40 -0300

    correções carta de apresentacao

--- app/pdfs/cartaapresentacao_pdf.rb
+++ app/pdfs/cartaapresentacao_pdf.rb
@@ -31,17 +31,17 @@ class CartaapresentacaoPdf< Prawn::Document
           if relatorio.matricula_aluno == @usuario_matricula
             draw_text "Campos dos Goytacazes, RJ, #{relatorio.data}", at: [240, 600], inline_format: true
             move_down 20
-            text "<b>DA:</b> Diretoria de Ensino Superior dos Cursos de Licenciatura do IFFluminense <i>Campus</i> Campos Centro (DIRESLCC)", align: :left, size: 12, inline_format: true
+            text "<b>DA:</b> Diretoria de Ensino Superior da Licenciaturas do IFFluminense <i>Campus</i> Campos Centro (DIRESLCC).", align: :left, size: 12, inline_format: true
             move_down 20
             text "<b>À:</b><b>Direção da Instituição de Ensino:</b> #{relatorio.instituicao_apresentacao}", align: :left, size: 12, inline_format: true
             move_down 10
-            text "Assunto: Apresentação do licenciando(a)-estagiário."
+            text "Assunto: Apresentação de licenciando(a)-estagiário."
             move_down 10
             text "Prezado Gestor.", align: :justify, inline_format: true, leading: 10
             move_down 5
-            text "O IFFluminense <i>Campus</i> Campos Centro apresenta o(a) licenciando(a)-estagiário(a): #{relatorio.aluno_apresentacao_dois}, matriculado(a), neste instituto, no #{relatorio.periodo} período do Curso Superior de Licenciatura em #{relatorio.licenciatura}, no #{relatorio.semestre_apresentacao} semestre letivo de #{relatorio.ano_apresentacao}, para fins de cumprimento das atividades do Estágio Curricular Supervisionado no campo de estágio dessa Instituição Concedente, previstas no Plano de Atividades de Estágio(PAE), conforme Calendário Acadêmico deste <i>campus</i>, em atendimento a carga horária total destinada ao período em curso.", align: :left, inline_format: true, leading: 10
+            text "O IFFluminense <i>Campus</i> Campos Centro apresenta o(a) licenciando(a)-estagiário(a) #{relatorio.aluno_apresentacao_dois}, matriculado(a) neste instituto no #{relatorio.periodo} período do Curso Superior de Licenciatura em #{relatorio.licenciatura}, no #{relatorio.semestre_apresentacao} semestre letivo de #{relatorio.ano_apresentacao}, para fins de cumprimento das atividades do Estágio Curricular Supervisionado no campo de estágio dessa Instituição Concedente, previstas no Plano de Atividades de Estágio(PAE), em atendimento à carga horária total destinada ao período em curso.", align: :left, inline_format: true, leading: 10
             move_down 10
-            text "Ressaltamos que o Estágio Curricular Supervisionado é componente obrigatório e de importância significativa para a formação dos futuros formadores. Agradecemos a atenção sempre nos concedida e a seriedade com que os profissionais da educação têm acolhido e acompanhado nossos(as) licenciados(as). Estamos à disposição para quaisquer esclarecimentos por meio dos contatos: (22) 2726-2897  e dirlicenciatura.camposcentro@iff.edu.br.", align: :left, inline_format: true, leading: 10
+            text "Ressaltamos que o Estágio Curricular Supervisionado é componente obrigatório, e não remunerado, e de importância significativa para a formação docente. Agradecemos a atenção sempre nos concedida e a seriedade com que os profissionais da educação têm acolhido e acompanhado nossos(as) licenciados(as). Estamos à disposição para quaisquer esclarecimentos por meio dos contatos: (22) 2726-2897  e dirlicenciatura.camposcentro@iff.edu.br.", align: :left, inline_format: true, leading: 10
             move_down 20
     
             text "Atenciosamente,", align: :center, inline_format: true, leading: 10
